91063b4d7a2b16b3b6470e6659b80d936f3c9e81
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-05-27 22:19:02 +0530

    Implement webhook handling and env loading

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,15 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/Aryan9inja/codebolt/internal/webhook"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+	secret := os.Getenv("GITHUB_WEBHOOK_SECRET")
+	if secret == "" {
+		log.Fatal("GITHUB_WEBHOOK_SECRET not set")
+	}
+
 	r := chi.NewRouter()
-	r.Post("/webhook", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	})
-	log.Println("CodeBolt listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	wh := webhook.NewHandler(secret)
+	r.Post("/webhook", wh.ServeHTTP)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("CodeBolt listening on :%s", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
