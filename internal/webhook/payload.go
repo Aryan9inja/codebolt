f839c5f327552dcb3ae222ae5fe7b1bbdb9b801c
Aryan Singh Thakur <aryana123b456@gmail.com>
2026-06-01 21:31:08 +0530

    Add PRReviewJobPayload for enqueuing jobs

--- internal/webhook/payload.go
+++ internal/webhook/payload.go
@@ -1,19 +1,19 @@
 package webhook
 
 type PullRequestEvent struct {
-	Action      string `json:"action"`
-	Number      int    `json:"number"`
-	PullRequest PullRequest `json:"pull_request"`
-	Repository Repository `json:"repository"`
+	Action       string       `json:"action"`
+	Number       int          `json:"number"`
+	PullRequest  PullRequest  `json:"pull_request"`
+	Repository   Repository   `json:"repository"`
 	Installation Installation `json:"installation"`
 }
 
 type PullRequest struct {
-	Number int `json:"number"`
-	Title string `json:"title"`
-	State string `json:"state"`
-	Head Ref `json:"head"`
-	Base Ref `json:"base"`
+	Number  int    `json:"number"`
+	Title   string `json:"title"`
+	State   string `json:"state"`
+	Head    Ref    `json:"head"`
+	Base    Ref    `json:"base"`
 	DiffURL string `json:"diff_url"`
 }
 
@@ -24,8 +24,8 @@ type Ref struct {
 
 type Repository struct {
 	FullName string `json:"full_name"`
-	Owner Owner `json:"owner"`
-	Name string `json:"name"`
+	Owner    Owner  `json:"owner"`
+	Name     string `json:"name"`
 }
 
 type Owner struct {
@@ -34,4 +34,14 @@ type Owner struct {
 
 type Installation struct {
 	ID int64 `json:"id"`
+}
+
+type PRReviewJobPayload struct {
+	InstallationID int64  `json:"installation_id"`
+	RepoFullName   string `json:"repo_full_name"`
+	Owner          string `json:"owner"`
+	RepoName       string `json:"repo_name"`
+	PRNumber       int    `json:"pr_number"`
+	HeadSHA        string `json:"head_sha"`
+	BaseSHA        string `json:"base_sha"`
 }
